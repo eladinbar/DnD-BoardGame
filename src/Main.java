6e3feb46fb89dae079fe1a16703351a423645882
Edan Levy <edal@post.bgu.ac.il>
2020-06-22 21:44:37 +0300

    position

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+
         CommandLineInterface CLI = new CommandLineInterface(args[0]);
         CLI.start();
         CLI.play();
