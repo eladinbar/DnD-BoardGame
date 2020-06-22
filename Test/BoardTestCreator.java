5ea2d41b867becf2ba16dda0f4cf47dedbc6188f
Edan Levy <edal@post.bgu.ac.il>
2020-06-22 20:32:33 +0300

    boardtest

--- Test/BoardTestCreator.java
+++ Test/BoardTestCreator.java
@@ -1,13 +1,15 @@
 import Controller.LevelCreationPackage.LevelCreator;
+import Model.UnitPackage.PlayerPackage.Player;
 import View.Level;
 
 public class BoardTestCreator {
 
     private LevelCreator levelCreator;
     private Level baseLevel;
+    private final String path = "D:\\UProject\\POOP\\DnD-BoardGame\\Test\\TestBoard.txt";
+    private Player player
 
-
-
-
-
+    public BoardTestCreator() {
+        this.levelCreator = new LevelCreator();
+    }
 }
